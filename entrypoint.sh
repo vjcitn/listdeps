288bb18c1e017cbc52a7e622bbc59f409aee212c
Vince Carey <stvjc@channing.harvard.edu>
2019-10-28 12:23:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Getting deps for $1"
 
-deps=`/usr/local/bin/Rscript -e "v=BiocManager::version(); library(BiocPkgTools); library(dplyr); library(rjson); z=filter(buildPkgDependencyDataFrame(), dependency=='$1'); toJSON(list(biocvers=as.character(v), query='$1', deps=z[[1]]))"`
+deps=`/usr/local/bin/Rscript -e "v=BiocManager::version(); library(BiocPkgTools); library(dplyr); library(rjson); z=filter(buildPkgDependencyDataFrame(), dependency=='$1'); cat(toJSON(list(biocvers=as.character(v), query='$1', deps=z[[1]])))"`
 
 #cat(toJSON(list(biocvers=as.character(v), query='$1', deps=z[[1]])))
 
