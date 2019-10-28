3ecef946b60c4877e180e2b11795ba82142da1c1
Vince Carey <stvjc@channing.harvard.edu>
2019-10-28 12:16:01 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ deps=`/usr/local/bin/Rscript -e "v=BiocManager::version(); library(BiocPkgTools)
 #cat(toJSON(list(biocvers=as.character(v), query='$1', deps=z[[1]])))
 
 #echo $deps
-echo ::set-output deps=$deps
+echo "::set-output name=deps::$deps"
 
